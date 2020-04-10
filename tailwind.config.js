fe36c89eeedfac21ce28705299e088b5e101af02
Julie Koubová <julie@koubova.net>
2020-04-10 11:11:55 +0200

    config line height

--- tailwind.config.js
+++ tailwind.config.js
@@ -59,6 +59,14 @@ module.exports = {
         screen: { raw: 'screen' },
         dark: { raw: 'screen and (prefers-color-scheme: dark)' }
       }
+    },
+    lineHeight: {
+      none: '1',
+      tight: '1.25',
+      snug: '1.375',
+      normal: '1.5',
+      relaxed: '1.625',
+      loose: '2',
     }
   },
   variants: {},
